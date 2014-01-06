05e1c50267f880837cf5ba3648f22ad97977a30c
Michael Irwin <mikesir87@gmail.com>
2014-01-06 10:50:03 -0500

    Added javascript and button to Pull Request page
    Issue #45
    
    Button only appears if the Pull Request is OPEN and the Pull Request is
    mergeable (conditions for the web-item)

---
+++ src/main/resources/static/jenkins-pr-triggerbutton.js
@@ -0,0 +1,30 @@
+
+require([
+  'jquery',
+  'aui',
+  'model/page-state'
+], function($, AJS, pageState) {
+
+  var resourceUrl = AJS.contextPath() + '/rest/jenkins/latest/projects/' 
+    + pageState.getProject().key + '/repos/' 
+    + pageState.getRepository().slug + '/triggerJenkins';
+
+  var waiting = '<span class="aui-icon aui-icon-wait">Wait</span>';
+  
+  $(".triggerJenkinsBuild").click(function() {
+    var $this = $(this);
+    var text = $this.text();
+
+    $this.attr("disabled", "disabled").html(waiting + " " + text);
+  
+    $.post(resourceUrl, function() {
+      // Place in timer for UI-happiness - might go "too quick" and not notice
+      // it actually triggered
+      setTimeout(function() {  
+          $this.removeAttr("disabled").text(text);
+      }, 500);
+    });
+    return false;
+  });
+
+});
