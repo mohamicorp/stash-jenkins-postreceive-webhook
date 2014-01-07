5c01d8f496e1ee731d80b1dfed204aa661279af9
Michael Irwin <mikesir87@gmail.com>
2014-01-07 09:08:26 -0500

    Created condition to verify webhook is enabled and configured it in
    Issue #45

---
+++ src/main/java/com/nerdwin15/stash/webhook/conditions/WebhookIsEnabledCondition.java
@@ -0,0 +1,55 @@
+package com.nerdwin15.stash.webhook.conditions;
+
+import java.util.Map;
+
+import com.atlassian.plugin.PluginParseException;
+import com.atlassian.plugin.web.Condition;
+import com.atlassian.stash.hook.repository.RepositoryHook;
+import com.atlassian.stash.repository.Repository;
+import com.atlassian.stash.setting.Settings;
+import com.nerdwin15.stash.webhook.service.SettingsService;
+
+/**
+ * A Condition that passes when the webhook is enabled for the provided
+ * repository.
+ *
+ * @author Michael Irwin (mikesir87)
+ */
+public class WebhookIsEnabledCondition implements Condition {
+
+  private static final String REPOSITORY = "repository";
+  
+  private SettingsService settingsService;
+  
+  /**
+   * Create a new instance of the condition
+   * @param settingsService The settings service
+   */
+  public WebhookIsEnabledCondition(SettingsService settingsService) {
+    this.settingsService = settingsService;
+  }
+  
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void init(Map<String, String> context) throws PluginParseException {
+    // Nothing to do here
+  }
+  
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean shouldDisplay(Map<String, Object> context) {
+    final Object obj = context.get(REPOSITORY);
+    if (!(obj instanceof Repository))
+      return false;
+    
+    Repository repo = (Repository) obj;
+    RepositoryHook hook = settingsService.getRepositoryHook(repo);
+    Settings settings = settingsService.getSettings(repo);
+    
+    return !(hook == null || !hook.isEnabled() || settings == null);
+  }
+}
---
+++ src/main/java/com/nerdwin15/stash/webhook/conditions/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains conditions for displaying of web fragments
+ */
+package com.nerdwin15.stash.webhook.conditions;
