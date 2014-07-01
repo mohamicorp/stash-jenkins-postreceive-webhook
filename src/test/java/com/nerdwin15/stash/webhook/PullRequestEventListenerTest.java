4538e0861927c29adfbf0e704b5ecbfb6b8568d0
Carl Loa Odin <carl.loa.odin@klarna.com>
2014-07-01 14:42:31 +0200

    Temporary: Will be rewritten with configuration
    - Deactivate triggering of webhook from opening and reopening of
      pull-requests.

--- src/test/java/com/nerdwin15/stash/webhook/PullRequestEventListenerTest.java
+++ src/test/java/com/nerdwin15/stash/webhook/PullRequestEventListenerTest.java
@@ -66,72 +66,72 @@ public class PullRequestEventListenerTest {
   /**
    * Validates that the notifier is used when the filter chain says ok
    */
-  @Test
-  public void shouldNotifyWhenChainSaysOk() throws Exception {
-    PullRequestRef r = Mockito.mock(PullRequestRef.class);
-    Mockito.when(r.toString()).thenReturn("project/repo:refs/heads/master");
-    Mockito.when(r.getLatestChangeset()).thenReturn("sha1");
-    when(event.getPullRequest().getFromRef()).thenReturn(r);
-
-    Settings settings = mock(Settings.class);
-
-    StashUser user = PowerMockito.mock(StashUser.class);
-    String username = "pinky";
-    ArgumentCaptor<EventContext> contextCaptor = 
-        ArgumentCaptor.forClass(EventContext.class);
-    when(event.getUser()).thenReturn(user);
-    when(user.getName()).thenReturn(username);
-    
-    when(settingsService.getSettings(repo)).thenReturn(settings);
-    when(filterChain.shouldDeliverNotification(contextCaptor.capture()))
-        .thenReturn(true);
-
-    listener.handleEvent(event);
-
-    verify(notifier).notifyBackground(repo, "master", "sha1");
-    assertEquals(event, contextCaptor.getValue().getEventSource());
-    assertEquals(username, contextCaptor.getValue().getUsername());
-    assertEquals(repo, contextCaptor.getValue().getRepository());
-  }
+//  @Test
+//  public void shouldNotifyWhenChainSaysOk() throws Exception {
+//    PullRequestRef r = Mockito.mock(PullRequestRef.class);
+//    Mockito.when(r.toString()).thenReturn("project/repo:refs/heads/master");
+//    Mockito.when(r.getLatestChangeset()).thenReturn("sha1");
+//    when(event.getPullRequest().getFromRef()).thenReturn(r);
+//
+//    Settings settings = mock(Settings.class);
+//
+//    StashUser user = PowerMockito.mock(StashUser.class);
+//    String username = "pinky";
+//    ArgumentCaptor<EventContext> contextCaptor =
+//        ArgumentCaptor.forClass(EventContext.class);
+//    when(event.getUser()).thenReturn(user);
+//    when(user.getName()).thenReturn(username);
+//
+//    when(settingsService.getSettings(repo)).thenReturn(settings);
+//    when(filterChain.shouldDeliverNotification(contextCaptor.capture()))
+//        .thenReturn(true);
+//
+//    listener.handleEvent(event);
+//
+//    verify(notifier).notifyBackground(repo, "master", "sha1");
+//    assertEquals(event, contextCaptor.getValue().getEventSource());
+//    assertEquals(username, contextCaptor.getValue().getUsername());
+//    assertEquals(repo, contextCaptor.getValue().getRepository());
+//  }
 
   /**
    * Validates that the notifier is not notified when the filter chain says no
    */
-  @Test
-  public void shouldNotifyWhenChainSaysCancel() throws Exception {
-    Repository repo = mock(Repository.class);
-    Settings settings = mock(Settings.class);
-    
-    StashUser user = mock(StashUser.class);
-    String username = "pinky";
-    ArgumentCaptor<EventContext> contextCaptor = 
-        ArgumentCaptor.forClass(EventContext.class);
-    when(event.getUser()).thenReturn(user);
-    when(user.getName()).thenReturn(username);
-
-    when(settingsService.getSettings(repo)).thenReturn(settings);
-    when(filterChain.shouldDeliverNotification(contextCaptor.capture()))
-        .thenReturn(false);
-
-    listener.handleEvent(event);
-
-    verify(notifier, never()).notifyBackground(repo, "master", "sha1");
-  }
+//  @Test
+//  public void shouldNotifyWhenChainSaysCancel() throws Exception {
+//    Repository repo = mock(Repository.class);
+//    Settings settings = mock(Settings.class);
+//
+//    StashUser user = mock(StashUser.class);
+//    String username = "pinky";
+//    ArgumentCaptor<EventContext> contextCaptor =
+//        ArgumentCaptor.forClass(EventContext.class);
+//    when(event.getUser()).thenReturn(user);
+//    when(user.getName()).thenReturn(username);
+//
+//    when(settingsService.getSettings(repo)).thenReturn(settings);
+//    when(filterChain.shouldDeliverNotification(contextCaptor.capture()))
+//        .thenReturn(false);
+//
+//    listener.handleEvent(event);
+//
+//    verify(notifier, never()).notifyBackground(repo, "master", "sha1");
+//  }
   
   /**
    * Validates that if the repository has no settings set, execution stops
    * @throws Exception
    */
-  @Test
-  public void shouldntConsultChainWhenSettingsAreNull() throws Exception {
-    RepositoryRefsChangedEvent e = mock(RepositoryRefsChangedEvent.class);
-    Repository repo = mock(Repository.class);
-
-    when(e.getRepository()).thenReturn(repo);
-    when(settingsService.getSettings(repo)).thenReturn(null);
-
-    listener.handleEvent(event);
-
-    verify(notifier, never()).notifyBackground(repo, "master", "sha1");
-  }
+//  @Test
+//  public void shouldntConsultChainWhenSettingsAreNull() throws Exception {
+//    RepositoryRefsChangedEvent e = mock(RepositoryRefsChangedEvent.class);
+//    Repository repo = mock(Repository.class);
+//
+//    when(e.getRepository()).thenReturn(repo);
+//    when(settingsService.getSettings(repo)).thenReturn(null);
+//
+//    listener.handleEvent(event);
+//
+//    verify(notifier, never()).notifyBackground(repo, "master", "sha1");
+//  }
 }
