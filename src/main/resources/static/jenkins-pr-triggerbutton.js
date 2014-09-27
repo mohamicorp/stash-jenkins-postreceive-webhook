96dd0f65b4fc53509374b72a22ac5f135c639abd
Michael Irwin <mikesir87@gmail.com>
2014-09-27 09:53:11 -0400

    Fixed Javascript error - Issue #75

--- src/main/resources/static/jenkins-pr-triggerbutton.js
+++ src/main/resources/static/jenkins-pr-triggerbutton.js
@@ -3,12 +3,14 @@ define('plugin/jenkins/pr-triggerbutton', [
   'aui',
   'model/page-state'
 ], function($, AJS, pageState) {
-
-  var resourceUrl = AJS.contextPath() + '/rest/jenkins/latest/projects/' 
-    + pageState.getProject().getKey() + '/repos/' 
-    + pageState.getRepository().getSlug() + '/triggerJenkins'
-    + '?branch=' + pageState.getPullRequest().getFromRef().getDisplayId()
-    + '&sha1=' + pageState.getPullRequest().getFromRef().getLatestChangeset();
+  
+  var getResourceUrl = function() {
+    return AJS.contextPath() + '/rest/jenkins/latest/projects/' 
+      + pageState.getProject().getKey() + '/repos/' 
+      + pageState.getRepository().getSlug() + '/triggerJenkins'
+      + '?branch=' + pageState.getPullRequest().getFromRef().getDisplayId()
+      + '&sha1=' + pageState.getPullRequest().getFromRef().getLatestChangeset();
+  };
 
   var waiting = '<span class="aui-icon aui-icon-wait">Wait</span>';
   
@@ -18,7 +20,7 @@ define('plugin/jenkins/pr-triggerbutton', [
 
     $this.attr("disabled", "disabled").html(waiting + " " + text);
   
-    $.post(resourceUrl, function() {
+    $.post(getResourceUrl(), function() {
       // Place in timer for UI-happiness - might go "too quick" and not notice
       // it actually triggered
       setTimeout(function() {  
