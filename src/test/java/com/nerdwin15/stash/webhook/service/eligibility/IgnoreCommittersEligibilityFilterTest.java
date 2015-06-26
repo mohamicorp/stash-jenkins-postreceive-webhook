90f45c23f6aaad98761d67d1f632260c273ef13f
Slawek Puklo <piszdomniejeszcze@gmail.com>
2015-06-26 19:13:32 +0100

    Changing ignored commiters list from being space delimited to be comma delimited.

--- src/test/java/com/nerdwin15/stash/webhook/service/eligibility/IgnoreCommittersEligibilityFilterTest.java
+++ src/test/java/com/nerdwin15/stash/webhook/service/eligibility/IgnoreCommittersEligibilityFilterTest.java
@@ -1,8 +1,8 @@
 package com.nerdwin15.stash.webhook.service.eligibility;
 
+import static com.nerdwin15.stash.webhook.service.eligibility.IgnoreCommittersEligibilityFilter.IGNORED_COMMITTERS_LIST_SPLITTING_CHARACTER;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.Before;
@@ -12,80 +12,142 @@ import com.atlassian.stash.repository.Repository;
 import com.atlassian.stash.setting.Settings;
 import com.nerdwin15.stash.webhook.Notifier;
 import com.nerdwin15.stash.webhook.service.SettingsService;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 /**
  * Test case for the {@link IgnoreCommittersEligibilityFilter} class
- * 
+ *
  * @author Michael Irwin (mikesir87)
  */
+@RunWith(MockitoJUnitRunner.class)
 public class IgnoreCommittersEligibilityFilterTest {
 
-  private SettingsService settingsService;
-  private IgnoreCommittersEligibilityFilter filter;
-  private Settings settings;
-  private Repository repo;
-  private EventContext eventContext;
-  private String username = "pinky";
-  
-  /**
-   * Setup tasks
-   */
-  @Before
-  public void setUp() throws Exception {
-    settingsService = mock(SettingsService.class);
-    repo = mock(Repository.class);
-    filter = new IgnoreCommittersEligibilityFilter(settingsService);
-    settings = mock(Settings.class);
-    when(settingsService.getSettings(repo)).thenReturn(settings);
-    
-    eventContext = mock(EventContext.class);
-    when(eventContext.getEventSource()).thenReturn(null);
-    when(eventContext.getRepository()).thenReturn(repo);
-    when(eventContext.getUsername()).thenReturn(username);
-  }
-  
-  /**
-   * Validate that the filter should still allow delivery when no ignored
-   * committers settings have been set.
-   * @throws Exception
-   */
-  @Test
-  public void shouldAllowWhenIgnoredCommittersNull() throws Exception {
-    when(settings.getString(Notifier.IGNORE_COMMITTERS)).thenReturn(null);
-    assertTrue(filter.shouldDeliverNotification(eventContext));
-  }
-  
-  /**
-   * Validate that the filter should still allow delivery when the event user
-   * does not match any of the ignored committers
-   * @throws Exception
-   */
-  @Test
-  public void shouldAllowWhenIgnoredCommittersDoesntMatch() throws Exception {
-    when(settings.getString(Notifier.IGNORE_COMMITTERS))
-      .thenReturn(username + "-notmatching");
-    assertTrue(filter.shouldDeliverNotification(eventContext));
-  }
-  
-  /**
-   * Validate that the filter should cancel if an ignored committer matches
-   * @throws Exception
-   */
-  @Test
-  public void shouldCancelWhenIgnoredCommittersMatches() throws Exception {
-    when(settings.getString(Notifier.IGNORE_COMMITTERS)).thenReturn(username);
-    assertFalse(filter.shouldDeliverNotification(eventContext));
-  }
-  
-  /**
-   * Validate that the filter should cancel if an ignored committer matches
-   * @throws Exception
-   */
-  @Test
-  public void shouldCancelWhenMatchesWithMultipleCommitters() throws Exception {
-    when(settings.getString(Notifier.IGNORE_COMMITTERS)).thenReturn(username 
-        + " anotherUser");
-    assertFalse(filter.shouldDeliverNotification(eventContext));
-  }
-  
+    private final String username = "Pinky";
+    private final String usernameWithSpace = "The Brain";
+    private final String spaces = "  ";
+
+    @Mock private SettingsService settingsService;
+
+    @Mock private Settings settings;
+
+    @Mock private Repository repo;
+
+    @Mock private EventContext eventContext;
+
+    private IgnoreCommittersEligibilityFilter filter;
+
+    /**
+     * Setup tasks
+     */
+    @Before
+    public void setUp() throws Exception {
+        when(settingsService.getSettings(repo)).thenReturn(settings);
+        when(eventContext.getEventSource()).thenReturn(null);
+        when(eventContext.getRepository()).thenReturn(repo);
+        when(eventContext.getUsername()).thenReturn(username);
+
+        filter = new IgnoreCommittersEligibilityFilter(settingsService);
+    }
+
+    /**
+     * Validate that the filter should still allow delivery when no ignored
+     * committers settings have been set.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void shouldAllowNotificationWhenIgnoredCommittersListIsNull() throws Exception {
+        when(settings.getString(Notifier.IGNORE_COMMITTERS))
+                .thenReturn(null);
+
+        assertTrue(filter.shouldDeliverNotification(eventContext));
+    }
+
+    /**
+     * Validate that the filter should still allow delivery when ignored
+     * committers settings have been set to empty string.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void shouldAllowNotificationWhenIgnoredCommittersListIsEmpty() throws Exception {
+        when(settings.getString(Notifier.IGNORE_COMMITTERS))
+                .thenReturn("");
+
+        assertTrue(filter.shouldDeliverNotification(eventContext));
+    }
+
+    /**
+     * Validate that the filter should still allow delivery when the event user
+     * does not match any of the ignored committers.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void shouldAllowNotificationWhenIgnoredCommittersDoesntMatch() throws Exception {
+        when(settings.getString(Notifier.IGNORE_COMMITTERS))
+                .thenReturn(username + "-notmatching");
+
+        assertTrue(filter.shouldDeliverNotification(eventContext));
+    }
+
+    /**
+     * Validate that the filter should not allow notification if an ignored committer matches.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void shouldNotAllowNotificationWhenIgnoredCommittersMatches() throws Exception {
+        when(settings.getString(Notifier.IGNORE_COMMITTERS))
+                .thenReturn(username);
+
+        assertFalse(filter.shouldDeliverNotification(eventContext));
+    }
+
+    /**
+     * Validate that the filter should not allow notification if an ignored committer matches,
+     * even if name is prefixed and suffixed with spaces.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void shouldNotAllowNotificationWhenIgnoredCommittersMatchesDespiteOfSpaces() throws Exception {
+        final String ignoredCommiter = spaces + username + spaces;
+        when(settings.getString(Notifier.IGNORE_COMMITTERS))
+                .thenReturn(ignoredCommiter);
+
+        assertFalse(filter.shouldDeliverNotification(eventContext));
+    }
+
+    /**
+     * Validate that the filter should not allow notification if one of ignored committers matches.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void shouldNotAllowNotificationWhenMatchesWithMultipleCommitters() throws Exception {
+        final String ignoredCommiters = username + IGNORED_COMMITTERS_LIST_SPLITTING_CHARACTER + usernameWithSpace;
+        when(settings.getString(Notifier.IGNORE_COMMITTERS))
+                .thenReturn(ignoredCommiters);
+
+        assertFalse(filter.shouldDeliverNotification(eventContext));
+    }
+
+    /**
+     * Validate that the filter should not allow notification if ignored committer matches
+     * and username of committer contains space.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void shouldNotAllowNotificationWhenMatchesUsernameContainingSpace() throws Exception {
+        when(eventContext.getUsername()).thenReturn(usernameWithSpace);
+        when(settings.getString(Notifier.IGNORE_COMMITTERS))
+                .thenReturn(usernameWithSpace);
+
+        assertFalse(filter.shouldDeliverNotification(eventContext));
+    }
+
 }
